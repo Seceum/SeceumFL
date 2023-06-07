c9908517222bffd31e73f426c9004788d7e399ca
Seceum <134931266+Seceum@users.noreply.github.com>
2023-06-07 13:41:36 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,13 +14,17 @@ release = '3.2'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['myst_parser']
 
 templates_path = ['_templates']
 exclude_patterns = []
 
 language = 'zh'
-
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
